c80066f011f3d3f09af16c960c05ee1cc8f6fbf2
senelb <senelbilgemen@gmail.com>
2022-09-28 23:39:28 +0300

    js eklendi

---
+++ yeni.js
@@ -0,0 +1,43 @@
+console.log("merhaba")
+console.log("2+2")
+//alert ("iyi bak hata varmı")
+console.log("mehaba","dünya","nasılsın");
+var firstname="melih";
+let lastname="bilgemen";
+const age =14;
+console.log(firstname,lastname,14);
+
+let Memleketi="samsun"
+console.log(Memleketi);
+console.log(age+6);
+let fil="senel";
+console.log(fil.length)
+console.log("firstname");
+console.log(Memleketi);
+console.log(age)
+console.log(lastname[5])
+let metin="Why do we use it?"
+console.log(metin[metin.length-1])
+let nam="kediler tatlıdır"
+console.log(nam.toLowerCase())
+console.log(nam.toUpperCase())
+console.log(nam.substring(3))
+console.log(nam.substring(0,3))
+console.log(nam.split(" "))
+console.log(nam.trim())
+console.log(nam.includes("i"))
+console.log(nam.charAt())
+let yeni ="yeni"+"dünya"
+let isim="merhaba"
+let ilkKarakter = isim.charAt(0).toUpperCase()
+let sonrakiler=isim.substring(1)
+console.log(sonrakiler)
+console.log()
+//alert ("selam")
+let b = 5;
+let a = 12;
+console.log(a+b);
+
+
+
+
