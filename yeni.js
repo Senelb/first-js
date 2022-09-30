6e4f921b6ab6ab50b4bb7d32ef06c597f1cf2eda
senelb <senelbilgemen@gmail.com>
2022-09-30 09:22:53 +0300

    yeni komutlar eklendi

--- yeni.js
+++ yeni.js
@@ -35,9 +35,89 @@ console.log(sonrakiler)
 console.log()
 //alert ("selam")
 let b = 5;
-let a = 12;
+let a = 10;
 console.log(a+b);
-
-
-
+console.log(a*b)
+let piNumber= (Math.PI);
+console.log(piNumber);
+console.log(Math.round(piNumber))
+console.log(Math.floor(piNumber))
+console.log(Math.ceil(piNumber))
+let randumNumber =Math.random();
+console.log(randumNumber *100)
+console.log(randumNumber *100)
+console.log(Math.floor(randumNumber * 100)+1)
+console.log(Math.ceil(randumNumber * 47)+1)
+console.log(Math.floor(randumNumber * 60)+1)
+let x= 18
+let y= 6
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log((x+y)*y)
+console.log(++x)
+console.log(--x)
+console.log(x%y)
+let net=15
+console.log(net)
+net=net +5
+console.log(net)
+net -=12
+console.log(net)
+console.log(x==y)
+console.log(x!==y)//eşit değilmi
+console.log(net)
+console.log(x==y)
+console.log(x===y)//tip ve değer olarak aynı olması gerekiyor
+let m="alma"
+let n=("elma")
+console.log(m==n)
+console.log(m===n)
+console.log(m!==n)
+let num1 =9;
+let num2 =11;
+console.log(num1 >num2);
+console.log(num1 <num2);
+console.log(num1 >=num2);
+console.log(num1 <=num2);
+console.log(true && true)
+console.log(false && true)
+console.log(false && false)
+console.log((num2 < 15)|| (num1 ===11))
+let isBig = num1 > 4 ? "evet büyük" : "yok ya"
+console.log(isBig)
+let newd=null
+num1 > 18 ? newd=15 : newd=25
+console.log(newd)
+let raining=true
+if (raining) 
+{ console.log("şemsiyeni al")}
+let c=17;
+let d=15;
+let f=12
+if (c>d && d===f) {
+    console.log (`${c} ${d}'den büyütür`)
+}else {
+    console.log(`${c} ${d}'den küçüktür`)
+}
+if(a>18){
+    console.log("dısarı çıkabilir")
+}else{
+    console.log("dısarı çıkamaz")
+}
+if(a>12 && a <18){
+    console.log("dısarı çık,uzaklaşma")
+}else if(a>=18){
+    console.log("kedilere mama ver")
+}else {
+    console.log("evde otur");
+}
+let j = 0
+if (j>0) {
+    j--
+} else {
+    j++
+}
+console.log(j)
 
