bdb5723ffdf8c4464bc02b730e4ee7103eaedfe8
senelb <senelbilgemen@gmail.com>
2022-10-05 23:54:03 +0300

    array calışmaları

---
+++ jv102.js
@@ -0,0 +1,44 @@
+let ad = "şenel"
+let soyad= "bilge"
+function kisi() {
+ return ad +"", soyad 
+}
+console.log(ad +" " +soyad)
+
+ arguman=15;
+const kedi=miyav =>miyav *7;
+console.log(kedi(arguman))
+let name='kedi'
+const logUserName = ()=>{
+    
+let isLoggedIn =true;
+let name ='Guest User';
+if (isLoggedIn){
+    let name ='DEMBELE';
+    console.log(name);
+}
+console.log(name);
+};
+logUserName()
+console.log(name);
+let myArr =["Merhaba", "Yeni", "Dünya"]
+console.log(myArr)
+console.log(myArr[0])
+console.log(myArr[2])
+let myArray =[];
+console.log(myArray);
+let newArr =new Array(3 ,"merhubu");
+console.log(newArr);
+newArr[0]="jupiter"
+let letArr=["mer","ha",5];
+let constArr=["mermer","halhal",45];
+letArr[1]="hayhay"
+constArr[1]="kaykay"
+console.log(letArr)
+console.log(constArr)
+let newArray =new Array(2)
+newArray[0]=3
+newArray[1]=5
+console.log(newArray)
+
+
